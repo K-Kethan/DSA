0e10104ac50357716453c054d11815ceaeb11b70
K Kethan <91373391+K-Kethan@users.noreply.github.com>
2022-07-21 19:26:54 +0530

    Add files via upload

---
+++ Recursion1.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+
+void print(int n){
+    if(n==1)
+    {
+        cout<<n<<" ";
+        return;
+    }
+    print(n-1);
+    cout<<n;
+}
+int main(){
+    int n;
+    cin>>n;
+    print(n);
+    return 0;
+}
