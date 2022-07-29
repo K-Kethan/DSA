0fb1de2c6becc1b0a68b25a8600b2f735aca809f
K Kethan <91373391+K-Kethan@users.noreply.github.com>
2022-07-29 20:48:18 +0530

    Add files via upload

---
+++ Greedy-PoliceThieves.cpp
@@ -0,0 +1,11 @@
+int maxThieves(char arr[],int n, int k)
+{
+    int count=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]=='P')
+        {
+            int x=max(0,i-k);
+        }
+    }
+}
