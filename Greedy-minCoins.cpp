1c85885361211df6aefdef3bc7505ffb9d6e7ea1
K Kethan <91373391+K-Kethan@users.noreply.github.com>
2022-07-30 20:26:36 +0530

    Add files via upload

---
+++ Greedy-minCoins.cpp
@@ -0,0 +1,23 @@
+int minCoins(int coin[], int n, int amount)
+{	
+    	sort(coin, coin + n);
+
+    	int res = 0;
+
+    	for(int i = n - 1; i >= 0; i--)
+    	{
+    		if(coin[i] <= amount)
+    		{
+    			int c = floor(amount / coin[i]);
+
+    			res = res + c;
+
+    			amount = amount - c * coin[i];
+    		}
+
+    		if(amount == 0)
+    			break;
+    	}
+
+    	return res;
+}
