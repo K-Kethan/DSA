d8ea6cb70ab83729786188a008e54ea887c64438
K Kethan <91373391+K-Kethan@users.noreply.github.com>
2022-07-31 23:16:55 +0530

    Add files via upload

---
+++ Hashing2.cpp
@@ -0,0 +1,24 @@
+void countFreq(int arr[], int n)
+{
+    // Mark all array elements as not visited
+    vector<bool> visited(n, false);
+ 
+    // Traverse through array elements and
+    // count frequencies
+    for (int i = 0; i < n; i++) {
+ 
+        // Skip this element if already processed
+        if (visited[i] == true)
+            continue;
+ 
+        // Count frequency
+        int count = 1;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                visited[j] = true;
+                count++;
+            }
+        }
+        cout << arr[i] << " " << count << endl;
+    }
+}
