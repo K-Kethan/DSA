59c470d97dbd3b088d2900936cdc5417aa14e208
K Kethan <91373391+K-Kethan@users.noreply.github.com>
2022-08-02 13:38:58 +0530

    Add files via upload

---
+++ Hashing5.cpp
@@ -0,0 +1,13 @@
+int pairWithSumX(int arr[],int n, int X)
+{
+    unordered_set<int> us;
+    for(int i = 0; i < n; i++)
+    {
+        if(us.find(X - arr[i]) != us.end())
+          return 1;
+          
+        us.insert(arr[i]);
+    }
+    return 0;
+    
+}
