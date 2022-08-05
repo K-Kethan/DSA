86911b6e532a4634f4b12178c0fadd6e8c9d7ea9
K Kethan <91373391+K-Kethan@users.noreply.github.com>
2022-08-05 16:29:14 +0530

    Add files via upload

---
+++ Hashing8.cpp
@@ -0,0 +1,16 @@
+
+
+int largestSubarrayWithSumX(int arr[], int n, int sum)
+{
+    int prefix_sum = 0;
+    unordered_set<int> us;
+    us.insert(0);
+    for(int i = 0; i < n; i++)
+    {
+        prefix_sum += arr[i];
+        if(us.find(prefix_sum - sum) != us.end())
+          return 1;
+        us.insert(prefix_sum);
+    }
+    return 0;
+}
