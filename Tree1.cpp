997bc7896a46f7218accdaf7d72058ebf743acc7
K Kethan <91373391+K-Kethan@users.noreply.github.com>
2022-08-10 22:35:08 +0530

    Add files via upload

---
+++ Tree1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node  
+{ 
+  int key; 
+  struct Node *left; 
+  struct Node *right; 
+  Node(int k){
+      key=k;
+      left=right=NULL;
+  }
+};
+
+void inorder(Node *root){
+    if(root!=NULL){
+        inorder(root->left);
+        cout<<root->key<<" ";
+        inorder(root->right);    
+    }
+}  
